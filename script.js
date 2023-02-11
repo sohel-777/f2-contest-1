e53a3df50310fa1e56824340d4605d92b73dd33c
SK SOHEL RAHAMAN <117460184+sohel-777@users.noreply.github.com>
2023-02-11 17:29:19 +0530

    code added

--- script.js
+++ script.js
@@ -8,20 +8,47 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+   arr.map(
+      function(arr){
+        if(arr.marks > 50){
+            console.log(arr);
+        }
+    });
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(
+        function(arra){
+          if(arra.marks > 50 ){
+           console.log(arra);
+          }
+      });
 }
 
 function addData() {
   //Write your code here, just console.log
+  var newData={id:4,name:"susan",age:"20",marks:45};
+    arr.push(newData);
+    console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  var updatedArr=arr.filter(remove);
+    function remove(arr){
+      return arr.marks >= 50;
+    }
+    console.log(updatedArr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+   var newArray= [
+      { id: 5, name: "tommy", age: "18", marks: 90 },
+      { id: 6, name: "arthur", age: "20", marks: 60 },
+      { id: 7, name: "polly", age: "26", marks: 85 },
+    ];
+    var finalArray= arr.concat(newArray);
+    console.log(finalArray);
 }
